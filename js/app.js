91697d4d48f6af8f9b04eba19b422b1ae962b2a4
Sylwia Fait <sylwia.fait@gmail.com>
2019-08-26 00:51:49 +0200

    Add gallery

--- js/app.js
+++ js/app.js
@@ -1,37 +1,54 @@
-function useScrollReveal() {
-    const slideLeft = {
-        distance: "150%",
-        origin: "left",
-        opacity: null
-    };
+function showPhoto() {
+    const photos = document.querySelectorAll(".photo");
 
-    const slideRight = {
-        distance: "150%",
-        origin: "right",
-        opacity: null
-    };
+    for (let i = 0; i < photos.length; i++) {
+        photos[i].addEventListener("click", function() {
+            const modal = this.querySelector(".modal");
 
-    // ScrollReveal().reveal(".main-header", { delay: 200 });
-    // ScrollReveal().reveal(".second-header", { delay: 300 });
-    // ScrollReveal().reveal(".story-header, .story-fourth-header, .story-paragraph, .about-us-box", slideLeft);
+            this.querySelector(".modal").classList.toggle("show-modal");
+
+            window.addEventListener("click", function(e) {
+                if (e.target == modal) {
+                    modal.style.display = "none";
+                }
+            })
+        })
+    }
 }
 
-function showAnimations() {
-    const mobile = window.matchMedia("screen and (min-width: 750px)");
+//     const slideLeft = {
+//         distance: "150%",
+//         origin: "left",
+//         opacity: null
+//     };
+//
+//     const slideRight = {
+//         distance: "150%",
+//         origin: "right",
+//         opacity: null
+//     };
+//
+//     ScrollReveal().reveal(".main-header", { delay: 200 });
+//     ScrollReveal().reveal(".second-header", { delay: 300 });
+//     ScrollReveal().reveal(".story-header, .story-fourth-header, .story-paragraph, .about-us-box", slideLeft);
+// }
 
-    if (mobile.matches) {
-        useScrollReveal();
-    }
-
-    mobile.addListener( function(mobile) {
-        if (mobile.matches) {
-            useScrollReveal();
-        }
-    });
-};
+// function showAnimations() {
+//     const mobile = window.matchMedia("screen and (min-width: 750px)");
+//
+//     if (mobile.matches) {
+//         useScrollReveal();
+//     }
+//
+//     mobile.addListener( function(mobile) {
+//         if (mobile.matches) {
+//             useScrollReveal();
+//         }
+//     });
+// };
 
 const init = () => {
-    // showPhoto();
+    showPhoto();
     // showAnimations();
 }
 
