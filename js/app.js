650eaa8242475d1030c0876d6e26b2f4bc9f1d43
Sylwia Fait <sylwia.fait@gmail.com>
2019-09-03 22:46:36 +0200

    Add animation

--- js/app.js
+++ js/app.js
@@ -28,41 +28,42 @@ function showPhoto() {
     }
 }
 
-//     const slideLeft = {
-//         distance: "150%",
-//         origin: "left",
-//         opacity: null
-//     };
-//
-//     const slideRight = {
-//         distance: "150%",
-//         origin: "right",
-//         opacity: null
-//     };
-//
-//     ScrollReveal().reveal(".main-header", { delay: 200 });
-//     ScrollReveal().reveal(".second-header", { delay: 300 });
-//     ScrollReveal().reveal(".story-header, .story-fourth-header, .story-paragraph, .about-us-box", slideLeft);
-// }
+function useScrollReveal() {
+    const slideLeft = {
+        distance: "150%",
+        origin: "left",
+        opacity: null
+    };
 
-// function showAnimations() {
-//     const mobile = window.matchMedia("screen and (min-width: 750px)");
-//
-//     if (mobile.matches) {
-//         useScrollReveal();
-//     }
-//
-//     mobile.addListener( function(mobile) {
-//         if (mobile.matches) {
-//             useScrollReveal();
-//         }
-//     });
-// };
+    const slideRight = {
+        distance: "150%",
+        origin: "right",
+        opacity: null
+    };
+
+    ScrollReveal().reveal(".animation-show", { delay: 400 });
+    ScrollReveal().reveal(".animation-left", slideLeft);
+    ScrollReveal().reveal(".animation-right", slideRight);
+}
+
+function showAnimations() {
+    const mobile = window.matchMedia("screen and (min-width: 1000px)");
+
+    if (mobile.matches) {
+        useScrollReveal();
+    }
+
+    mobile.addListener( function(mobile) {
+        if (mobile.matches) {
+            useScrollReveal();
+        }
+    });
+};
 
 const init = () => {
     showHamburgerMenu();
     showPhoto();
-    // showAnimations();
+    showAnimations();
 }
 
 document.addEventListener("DOMContentLoaded", init);
